43483970b697199ebaca10e875448eafa7f3c369
CohenLab at Princeton <56199891+CohenLabPrinceton@users.noreply.github.com>
2021-08-12 10:34:09 -0400

    Update constants.py

--- Training_Testing_Output_Code/fishandra/constants.py
+++ Training_Testing_Output_Code/fishandra/constants.py
@@ -1,3 +1,6 @@
+# This script was produced by the Polavieja Lab. Original source code can be found here:
+#   https://gitlab.com/polavieja_lab/fishandra
+
 import os
 dir_of_executable = os.path.dirname(__file__)
 path_to_project_root = os.path.abspath(dir_of_executable)
