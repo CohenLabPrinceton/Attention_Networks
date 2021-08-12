4a8ed47df7e99a8591ce6eaedd6792d7c0aad1fb
CohenLab at Princeton <56199891+CohenLabPrinceton@users.noreply.github.com>
2021-08-12 10:35:41 -0400

    Update sample_with_model.py

--- Training_Testing_Output_Code/fishandra/sample_with_model.py
+++ Training_Testing_Output_Code/fishandra/sample_with_model.py
@@ -1,3 +1,6 @@
+# This script was produced by the Polavieja Lab. Original source code can be found here:
+#   https://gitlab.com/polavieja_lab/fishandra
+
 import os
 import numpy as np
 import logging
