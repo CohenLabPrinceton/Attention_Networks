836162650388c1604ca40404628d6b67b28a2916
CohenLab at Princeton <56199891+CohenLabPrinceton@users.noreply.github.com>
2021-08-12 10:33:55 -0400

    Update __init__.py

--- Training_Testing_Output_Code/fishandra/__init__.py
+++ Training_Testing_Output_Code/fishandra/__init__.py
@@ -1,2 +1,5 @@
+# This script was produced by the Polavieja Lab. Original source code can be found here:
+#   https://gitlab.com/polavieja_lab/fishandra
+
 import pickle
 pickle.DEFAULT_PROTOCOL = 4
